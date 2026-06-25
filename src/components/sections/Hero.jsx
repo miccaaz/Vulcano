f4c240a7bef59e36ace5efd4ad899ecb3b5146e9
Miguel <mclcastroloureiro@gmail.com>
2026-06-25 18:03:33 -0300

    Adiciona estilo de hover na seção Hero e cria arquivo de dados para jogos

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
 
         {/* Infos */}
         <div className='flex flex-col gap-4'>
-          <div className='flex gap-4'>
+          <div className='flex gap-4 *:hover:scale-105'>
             <div className='bg-gray rounded-2xl p-6 w-96'>
               <h3 className='text-3xl mb-12'>TOTAL GAMES</h3>
               <span className='flex justify-end text-8xl'>20</span>
@@ -20,14 +20,14 @@ const Hero = () => {
             </div>
           </div>
           
-          <div className='flex justify-between bg-gray text-3xl rounded-2xl p-7'>
+          <div className='flex justify-between bg-gray text-3xl rounded-2xl p-7 hover:scale-105'>
             <h3>MOST PLAYED PLATAFORM:</h3>
             <h3>STEAM</h3>
           </div>
         </div>
 
         {/* Favorite Game */}
-        <div className='bg-gray rounded-2xl p-2'>
+        <div className='bg-gray rounded-2xl p-2 hover:scale-105'>
           <img src="/images/hollow-knight-cover.png" className='rounded-xl'/>
           <article className='flex justify-between text-xl py-6'>
             <h3>FAVORITE GAME: HOLLOW KNIGHT</h3>
---
+++ src/data/games.js
@@ -0,0 +1,11 @@
+export const games = [
+  {
+    id: 1,
+    title: '',
+    image: '',
+    plataform: '',
+    rating: '',
+    genres: [''],
+    status: ''
+  }
+]
